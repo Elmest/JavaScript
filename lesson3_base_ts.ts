e196618ed865ef1a158dc5b1875341753354f00d
Dmitry Shkutnik <9851123@mai.ru>
2022-06-16 23:04:53 +0300

    2 tasks

---
+++ lesson3_base_ts.ts
@@ -0,0 +1,32 @@
+//Type script tasks:
+
+//Task 1. Создайте переменные, затем сложите их и выведите результат в консоль разработчика.
+
+let x : number = 20;
+let y : number = 58;
+let z : number = 42;
+
+console.log(x+y+z);
+
+// //Task 2 
+// Создайте переменные:
+// - количество секунд в минуте
+// - количество минут в часу
+// - количество часов в сутках
+// - количество суток в году
+// Посчитайте ваш возраст в секундах и поместите результат в переменную myAgeInSeconds
+
+
+//Task 3 
+// Создайте две переменные. Поместите в них переменную count и превратите в строку, 
+// а userName наоборот в число. Попробуйте реализовать задачу двумя разными способами.
+
+let count : number = 42;
+let countString : string = String(count);
+
+let userName : string = "42";
+let userNameNumber : number = Number(userName);
+
+console.log(typeof countString);
+console.log(typeof userNameNumber);
+
